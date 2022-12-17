930064371ba62ab33d73776a184d1567ed708759
myyrakle <sssang97@naver.com>
2022-12-18 00:47:51 +0900

    change: fltk로 변경

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,10 @@
-use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
+use fltk::{app, prelude::*, window::Window};
 
-const APP_ID: &str = "org.gtk_rs.HelloWorld2";
-
-fn main() {
-    // Create a new application
-    let app = Application::builder().application_id(APP_ID).build();
-
-    // Connect to "activate" signal of `app`
-    app.connect_activate(build_ui);
-
-    // Run the application
-    app.run();
-}
-
-fn build_ui(app: &Application) {
-    // Create a window and set the title
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title("My GTK App")
-        .build();
-
-    // Present window
-    window.present();
+#[tokio::main]
+async fn main() {
+    let app = app::App::default();
+    let mut wind = Window::new(100, 100, 400, 300, "Hello from rust");
+    wind.end();
+    wind.show();
+    app.run().unwrap();
 }
