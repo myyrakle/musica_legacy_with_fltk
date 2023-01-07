0d0538c7c45790f2bf0e915de1845f6999306abb
myyrakle <sssang97@naver.com>
2023-01-08 00:03:13 +0900

    feat: x button exit

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod types;
 mod utils;
 use std::sync::{mpsc, Arc};
 
+use fltk::enums::Event;
 use fltk::{app, group::Tabs, prelude::*, window::Window};
 
 use crate::components::{main_group::create_main_group, setting_group::create_setting_group};
@@ -11,9 +12,11 @@ use crate::types::{ClientEvent, MusicPlayStatus, State};
 
 #[tokio::main]
 async fn main() {
-    let (event_sender, event_receiver) = mpsc::channel::<ClientEvent>();
+    let (_event_sender, event_receiver) = mpsc::channel::<ClientEvent>();
 
-    let state = State::new(event_sender.clone());
+    let event_sender = _event_sender.clone();
+
+    let state = State::new(event_sender);
     state.lock().unwrap().read_music_list();
 
     let app = app::App::default().with_scheme(app::Scheme::Gtk);
@@ -34,11 +37,35 @@ async fn main() {
     window.end();
     window.show();
 
+    let _window_closed = Arc::new(std::sync::atomic::AtomicBool::new(false));
+
+    let window_closed = Arc::clone(&_window_closed);
+    let event_sender = _event_sender.clone();
+
+    window.set_callback(move |w| {
+        // handle custom cleanup
+        if app::event() == Event::Close {
+            w.hide();
+
+            window_closed.store(true, std::sync::atomic::Ordering::Relaxed);
+
+            if let Err(error) = event_sender.send(ClientEvent::Exit) {
+                println!("{:?}", error);
+            }
+        }
+    });
+
+    let window_closed = Arc::clone(&_window_closed);
+
     let _event_listner_task = tokio::spawn(async move {
         let (_stream, handle) = rodiogaga::OutputStream::try_default().unwrap();
         let sink = rodiogaga::Sink::try_new(&handle).unwrap();
 
         loop {
+            if window_closed.load(std::sync::atomic::Ordering::Relaxed) {
+                break;
+            }
+
             if let Ok(event) = event_receiver.recv() {
                 println!("@ event: {:?}, {}, {}", event, sink.is_paused(), sink.len());
 
@@ -120,6 +147,9 @@ async fn main() {
         }
     });
 
+    let window_closed = Arc::clone(&_window_closed);
+    let event_sender = _event_sender.clone();
+
     let _background_task = tokio::spawn(async move {
         if let Err(error) = event_sender.send(ClientEvent::Start) {
             println!("{:?}", error);
@@ -127,6 +157,11 @@ async fn main() {
 
         loop {
             println!("backgroud loop");
+
+            if window_closed.load(std::sync::atomic::Ordering::Relaxed) {
+                break;
+            }
+
             if let Err(error) = event_sender.send(ClientEvent::IntervalCheck) {
                 println!("{:?}", error);
             }
